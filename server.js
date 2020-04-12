b5d2de3a50f5f3078f91d35d08eee03f6c3d5a08
peterboldyrev <53663056+peterboldyrev@users.noreply.github.com>
2020-04-12 10:52:24 -0700

    adding minor fixes for heroku

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var path = require("path");
 // =============================================================
 var app = express();
 // var PORT = 3000;
-let port = process.env.PORT || 8080
+let port = process.env.PORT || 8080;
 
 
 // Sets up the Express app to handle data parsing
