9f53780c8510f83b786f7d928cd42fc618ceb6bc
peterboldyrev <53663056+peterboldyrev@users.noreply.github.com>
2020-04-12 10:51:43 -0700

    added let port = process.env.PORT || 8080 for heroku

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+// var PORT = 3000;
+let port = process.env.PORT || 8080
+
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -71,6 +73,6 @@ app.post("/api/tableList", function(req, res) {
 
 // Starts the server to begin listening
 // =============================================================
-app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
+app.listen(port, function() {
+  console.log("App listening on PORT " + port);
 });
